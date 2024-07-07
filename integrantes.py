7fad187b56126ceeccb540a079b0f1cfdea17080
root <root@labvm.vm>
2024-07-07 18:54:07 +0000

    Python de Integrantes

---
+++ integrantes.py
@@ -0,0 +1,7 @@
+# Lista de nombres y apellidos
+integrantes = ["Abraham Cabrera", "Jordan Payacan", "Rodrigo Cofre"]
+
+# Imprimir la lista de nombres y apellidos
+for integrante in integrantes:
+    print(integrante)
+
